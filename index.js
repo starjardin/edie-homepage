367b111d64b8f9ed9593194b0c9d54b52b1661d0
Tantely Andrianarivola <tantely.and@onja.org>
2020-11-28 13:33:32 +0300

    corrected font-name

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const navTemplate = document.querySelector(".template");
 const headerNav = document.querySelector(".header__navigation")
 const footer = document.querySelector(".footer");
 const footerNav = footer.querySelector(".footer__navigation");
+const footerFormContact = footer.querySelector('.footer__form-contact')
 const headerNavContent = document.importNode(navTemplate.content, true)
 const footerNavContent = document.importNode(navTemplate.content, true)
-const footerFormContact = footer.querySelector('.footer__form-contact')
-const fromContactTemplate = document.querySelector(".form-template")
+const formContactTemplate = document.querySelector(".form-template")
 const purposeForm = document.querySelector('.purpose__container')
 
 footerNav.appendChild(headerNavContent);
 headerNav.appendChild(footerNavContent);
-footerFormContact.appendChild(document.importNode(fromContactTemplate.content, true))
-purposeForm.appendChild(document.importNode(fromContactTemplate.content, true))
+footerFormContact.appendChild(document.importNode(formContactTemplate.content, true))
+purposeForm.appendChild(document.importNode(formContactTemplate.content, true))
